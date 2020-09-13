64b098c4492c5d405ce236603a6250b5ca8fce0c
Orchaldir <orchaldir@gmail.com>
2020-09-13 20:26:58 +0200

    [#2] Add AttitudeComponent.getAttitudesOfType()

--- core/src/main/kotlin/social/data/character/attitude/AttitudeComponent.kt
+++ core/src/main/kotlin/social/data/character/attitude/AttitudeComponent.kt
@@ -5,4 +5,8 @@ import social.data.character.CharacterId
 data class AttitudeComponent(val attitudes: Map<CharacterId, List<Attitude>>) {
 
     fun getAttitudesTowards(id: CharacterId) = attitudes.getOrDefault(id, emptyList())
+
+    fun getAttitudesOfType(type: AttitudeType) = attitudes.
+        mapValues { it.value.filter { it.type == type } }.
+        filterValues { it.isNotEmpty() }
 }
--- core/src/test/kotlin/social/data/character/attitude/AttitudeComponentTest.kt
+++ core/src/test/kotlin/social/data/character/attitude/AttitudeComponentTest.kt
@@ -3,30 +3,59 @@ package social.data.character.attitude
 import assertk.assertThat
 import assertk.assertions.isEmpty
 import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import social.data.character.CharacterId
 
 internal class AttitudeComponentTest {
 
-    private val type = AttitudeType("type", 0)
+    private val type0 = AttitudeType("type0", 0)
+    private val type1 = AttitudeType("type1", 0)
 
     private val id0: CharacterId = 0
     private val id1: CharacterId = 1
     private val id2: CharacterId = 2
 
-    private val attitudes0 = listOf(Attitude(type, 0), Attitude(type, 2))
-    private val attitudes1 = listOf(Attitude(type, 3))
+    private val attitude0 = Attitude(type0, 0)
+    private val attitude1 = Attitude(type1, 2)
+    private val attitude2 = Attitude(type0, 3)
+
+    private val attitudes0 = listOf(attitude0, attitude1)
+    private val attitudes1 = listOf(attitude2)
 
     private val component = AttitudeComponent(mapOf(id0 to attitudes0, id1 to attitudes1))
 
-    @Test
-    fun `Get attitudes towards a character`() {
-        assertThat(component.getAttitudesTowards(id0)).isEqualTo(attitudes0)
-        assertThat(component.getAttitudesTowards(id1)).isEqualTo(attitudes1)
+    @Nested
+    inner class GetAttitudesTowards {
+
+        @Test
+        fun `Get attitudes towards a character`() {
+            assertThat(component.getAttitudesTowards(id0)).isEqualTo(attitudes0)
+            assertThat(component.getAttitudesTowards(id1)).isEqualTo(attitudes1)
+        }
+
+        @Test
+        fun `Character has no attitudes towards another character`() {
+            assertThat(component.getAttitudesTowards(id2)).isEmpty()
+        }
     }
 
-    @Test
-    fun `Character has no attitudes towards another character`() {
-        assertThat(component.getAttitudesTowards(id2)).isEmpty()
+    @Nested
+    inner class GetAttitudesOfType {
+
+        @Test
+        fun `Get all attitudes of a type`() {
+            assertThat(component.getAttitudesOfType(type0)).isEqualTo(
+                mapOf(
+                    id0 to listOf(attitude0),
+                    id1 to listOf(attitude2)
+                )
+            )
+        }
+
+        @Test
+        fun `Character has no attitude of that type to another character`() {
+            assertThat(component.getAttitudesOfType(type1)).isEqualTo(mapOf(id0 to listOf(attitude1)))
+        }
     }
 }
