7a7ab5bb42e0683835b5dad7b548f3254eee895e
Orchaldir <orchaldir@gmail.com>
2020-09-18 14:10:42 +0200

    [#4] Refactor ModifyAttitude

--- core/src/main/kotlin/social/data/utility/effect/ModifyAttitude.kt
+++ core/src/main/kotlin/social/data/utility/effect/ModifyAttitude.kt
@@ -1,10 +1,17 @@
 package social.data.utility.effect
 
+import social.data.character.Character
 import social.data.character.attitude.Attitude
 import social.data.character.attitude.AttitudeComponent
 import social.data.character.attitude.AttitudeType
 import social.data.utility.CharacterRole
 import social.data.utility.Update
+
+private fun <K, V> Map<K, V>.copyAndAdd(key: K, value: V): Map<K, V> {
+    val newMap = HashMap(this)
+    newMap[key] = value
+    return newMap
+}
 
 data class ModifyAttitude(
     val from: CharacterRole,
@@ -17,23 +24,29 @@ data class ModifyAttitude(
         val fromCharacter = requireNotNull(update.getCharacter(from))
         val towardCharacter = requireNotNull(update.getCharacter(toward))
 
-        val oldAttitude = fromCharacter.attitudeComponent.getAttitude(towardCharacter.id, type)
-        val oldValue = oldAttitude?.value ?: type.defaultValue
-        val newValue = oldValue + modifier
-        val newAttitude = Attitude(type, newValue)
+        val newAttitude = updateAttitude(fromCharacter, towardCharacter)
 
-        val newAttitudesToward = HashMap(fromCharacter.attitudeComponent.attitudes[fromCharacter.id] ?: emptyMap())
-        newAttitudesToward[type] = newAttitude
+        val newAttitudesTowardCharacter = (fromCharacter.attitudeComponent.attitudes[fromCharacter.id] ?: emptyMap())
+            .copyAndAdd(type, newAttitude)
 
-        val newAttitudes = HashMap(fromCharacter.attitudeComponent.attitudes)
-        newAttitudes[towardCharacter.id] = newAttitudesToward
+        val newAttitudes = fromCharacter.attitudeComponent.attitudes
+            .copyAndAdd(towardCharacter.id, newAttitudesTowardCharacter)
 
         val newFromCharacter = fromCharacter.copy(attitudeComponent = AttitudeComponent(newAttitudes))
 
-        val newUpdatedCharacters = HashMap(update.updatedCharacters)
-        newUpdatedCharacters[from] = newFromCharacter
+        val newUpdatedCharacters = update.updatedCharacters.copyAndAdd(from, newFromCharacter)
 
         return update.copy(updatedCharacters = newUpdatedCharacters)
+    }
+
+    private fun updateAttitude(
+        fromCharacter: Character,
+        towardCharacter: Character
+    ): Attitude {
+        val oldAttitude = fromCharacter.attitudeComponent.getAttitude(towardCharacter.id, type)
+        val oldValue = oldAttitude?.value ?: type.defaultValue
+        val newValue = oldValue + modifier
+        return Attitude(type, newValue)
     }
 
 }
