020a0f79d5068195d968cb95f3605ef1f39ce4bf
Orchaldir <orchaldir@gmail.com>
2020-09-18 19:39:37 +0200

    [#5] Refactor Action

--- core/src/main/kotlin/social/data/action/Action.kt
+++ core/src/main/kotlin/social/data/action/Action.kt
@@ -3,13 +3,8 @@ package social.data.action
 import social.data.utility.Context
 import social.data.utility.effect.Effect
 
-class Action(val name: String, val utilityRules: List<UtilityRule>, val effects: List<Effect>) {
+class Action(val name: String, private val utilityRules: List<UtilityRule>, private val effects: List<Effect>) {
 
-    fun calculateUtility(context: Context): Int {
-        var utility = 0
-
-        utilityRules.forEach { r -> utility += r.getUtilityModifier(context) }
-
-        return utility
-    }
+    fun calculateUtility(context: Context) = utilityRules
+        .fold(0) { utility, rule -> utility + rule.getUtilityModifier(context) }
 }
