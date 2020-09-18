8349fbadd1be14236f97b8f81a6dbb873cdbeb77
Orchaldir <orchaldir@gmail.com>
2020-09-18 14:23:11 +0200

    [#7] Fix main()

--- app/src/main/kotlin/main.kt
+++ app/src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 import social.data.character.Character
+import social.data.character.attitude.AttitudeComponent
 
 fun main() {
-    val character = Character(99)
+    val character = Character(99, AttitudeComponent(emptyMap()))
     println("Hello $character")
 }
